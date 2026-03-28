d3ff979c25810500e7e9fe2f58c2b766571f78b1
Shench35 <akinpelushuaib0@gmail.com>
2026-03-28 21:25:35 +0100

    message

---
+++ src/mail.py
@@ -0,0 +1,33 @@
+from fastapi_mail import FastMail, ConnectionConfig, MessageSchema, MessageType
+from  src.app.config import Config
+from pathlib import Path
+from typing import List
+
+BASE_DIR = Path(__file__).resolve().parent
+
+mail_config = ConnectionConfig(
+    MAIL_USERNAME =Config.MAIL_USERNAME,
+    MAIL_PASSWORD = Config.MAIL_PASSWORD,
+    MAIL_FROM = Config.MAIL_FROM,
+    MAIL_PORT = Config.MAIL_PORT,
+    MAIL_SERVER = Config.MAIL_SERVER,
+    MAIL_STARTTLS = True,
+    MAIL_SSL_TLS = False,
+    USE_CREDENTIALS = True,
+    VALIDATE_CERTS = True,
+    TEMPLATE_FOLDER= Path(BASE_DIR, "template")
+)
+
+
+mail = FastMail(config=mail_config)
+
+def create_message(recipients:List[str], subject:str, body:str):
+    message = MessageSchema(
+        recipients= recipients,
+        subject=subject,
+        body=body,
+        subtype=MessageType.html
+    )
+
+    return message
+
